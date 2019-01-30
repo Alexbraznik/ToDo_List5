e9cabefacb233d26ae70a513b16a1411b4f653a8
Alex Braznik <Alexbraznik55@gmail.com>
2019-01-30 22:35:45 +0300

    30.01.2019

--- ToDoList/App_Start/Startup.Auth.cs
+++ ToDoList/App_Start/Startup.Auth.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Owin;
-using ToDoList.Models;
-using static ToDoList.Controllers.ToDoesController;
 
 namespace ToDoList
 {
@@ -14,27 +8,9 @@ namespace ToDoList
     {
         public void ConfigureAuth(IAppBuilder app)
         {
-
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login"),
-                Provider = new CookieAuthenticationProvider
-                {
- 
-                }
-            });            
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
             app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
-
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
-
-
         }
-    }
-
-    internal class ApplicationUserManager
-    {
     }
 }
--- ToDoList/Controllers/ToDoesController.cs
+++ ToDoList/Controllers/ToDoesController.cs
@@ -1,13 +1,5 @@
-﻿using Microsoft.AspNet.Identity;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 using ToDoList.Models;
 
@@ -15,40 +7,16 @@ namespace ToDoList.Controllers
 {
     public class ToDoesController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
-        private object toDo;
-        private ApplicationUser currentUser;
-        private IEnumerable<ToDo> myToDoes;
-
-        public IEnumerable<ToDo> MyToDoes { get => myToDoes; set => myToDoes = value; }
-        public object ToDo { get => toDo; set => toDo = value; }
-        internal ApplicationUser CurrentUser { get => currentUser; set => currentUser = value; }
-
         public ActionResult Index()
         {
-            var model = new ToDo { };
-            return View("Index", model);
-        }
-
-        private IEnumerable<ToDo> GetMyToDoes()
-        {
-            string currentUserId = User.Identity.GetUserId();
-
-
-            int completeCount = 0;
-            {
-                {
-                    completeCount++;
-                }
-            }
-
-
-            return MyToDoes;
+            var model = new ToDo();
+            return View(model);
         }
 
         public ActionResult BuildToDoTable()
         {
-            return PartialView("_ToDoTable", GetMyToDoes());
+            var emptyList = new List<ToDo>();
+            return PartialView("_ToDoTable", emptyList);
         }
 
         public ActionResult Details(int? id)
@@ -57,9 +25,8 @@ namespace ToDoList.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            {
-                return HttpNotFound();
-            }
+
+            return HttpNotFound();
         }
 
         public ActionResult Create()
@@ -83,13 +50,8 @@ namespace ToDoList.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult AJAXCreate([Bind(Include = "Id,Description")] ToDo toDo)
         {
-            if (ModelState.IsValid)
-            {
-
-
-            }
-
-            return PartialView("_ToDoTable", GetMyToDoes());
+            var emptyList = new List<ToDo>();
+            return PartialView("_ToDoTable", emptyList);
         }
 
         public ActionResult Edit(int? id)
@@ -99,17 +61,7 @@ namespace ToDoList.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            if (ToDo == null)
-            {
-                return HttpNotFound();
-            }
-
-            string currentUserId = User.Identity.GetUserId();
-
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
+            return View();
         }
 
         [HttpPost]
@@ -118,9 +70,9 @@ namespace ToDoList.Controllers
         {
             if (ModelState.IsValid)
             {
-                db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
             return View(toDo);
         }
 
@@ -131,13 +83,8 @@ namespace ToDoList.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            {
-                return HttpNotFound();
-            }
-            {
-            }
 
-
+            return HttpNotFound();
         }
 
         public ActionResult Delete(int? id)
@@ -146,46 +93,8 @@ namespace ToDoList.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            return ToDo == null ? HttpNotFound() : (ActionResult)View(ToDo);
-        }
 
-        [ValidateAntiForgeryToken]
-
-
-
-
-
-
-        internal class ApplicationUser
-        {
-            internal Task<ClaimsIdentity> GenerateUserIdentityAsync(ApplicationUserManager manager)
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        internal class ApplicationDbContext
-        {
-
-            internal void Dispose()
-            {
-                throw new NotImplementedException();
-            }
-
-            internal object Entry(ToDo toDo)
-            {
-                throw new NotImplementedException();
-            }
-
-            internal object Entry(object toDo)
-            {
-                throw new NotImplementedException();
-            }
-
-            internal void SaveChanges()
-            {
-                throw new NotImplementedException();
-            }
+            return View("Index");
         }
     }
 }
